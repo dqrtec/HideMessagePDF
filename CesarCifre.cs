db2a6cbbed595044e1c32c6f696d21548d6a9df7
dqrtec <dqrte@hotmail.com>
2024-11-24 18:10:15 -0300

    add main files

---
+++ CesarCifre.cs
@@ -0,0 +1,33 @@
+﻿namespace HidePDF
+{
+    public static class CesarCifre
+    {
+        public static string Encrypt(this string message, int shift = 5)
+        {
+            string newMessage = string.Empty;
+
+            foreach (char c in message)
+            {
+                char newChar = (char)(((byte)c) + ((byte)shift));
+
+                newMessage += newChar;
+            }
+
+            return newMessage;
+        }
+
+        public static string Dencrypt(this string message, int shift = 5)
+        {
+            string newMessage = string.Empty;
+
+            foreach (char c in message)
+            {
+                char newChar = (char)(((byte)c) - ((byte)shift));
+
+                newMessage += newChar;
+            }
+
+            return newMessage;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.Annotations;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Quality;
+
+namespace HidePDF
+{
+    public partial class Form1 : Form
+    {
+        string namefile = "teste1.pdf";
+        public Form1()
+        {
+            InitializeComponent();
+
+            string stringEscondida = "fenix-55123".Encrypt();
+
+            CreatePDF(namefile, stringEscondida);
+            
+            ReadPDF(namefile);
+        }
+
+        void ReadPDF(string namefile)
+        {
+            var document = PdfReader.Open(namefile);
+
+            var secretMessage = document.Info.Title.Dencrypt();
+
+            MessageBox.Show(secretMessage+"\n"+ document.Info.Title);
+        }
+
+        void CreatePDF(string namefile, string stringEscondida)
+        {
+            var document = new PdfDocument(namefile);
+
+            document.Info.Title = stringEscondida;
+
+            var page = document.AddPage();
+
+            var textAnnot = new PdfTextAnnotation
+            {
+                Title = "Escondido".Encrypt(),
+                Subject = "Escondido".Encrypt(),
+                Contents = "Escondido".Encrypt(),
+                Icon = PdfTextAnnotationIcon.Help,
+                Color = XColors.LimeGreen,
+                Opacity = 1,
+                Open = true
+            };
+
+            page.Annotations.Add(textAnnot);
+
+            document.Close();
+        }
+    }
+}
